4c00de8963a3b4aa9f88f66994976243570c93dd
Hemant <hemantkumarsonkar1@gmail.com>
2024-02-02 17:28:50 +0530

    array map

---
+++ javaScript2/ArrayCallback/script.js
@@ -0,0 +1,46 @@
+// ---------------- Array Callback Functions ---------------
+
+// 1 ForEach ->
+
+// const arr = [5, 8, 99, 100, 240, 6, 93, 64];
+// console.log(arr);
+
+// function hemantWants(item){
+//     console.log(item);
+// }
+
+// arr.forEach(hemantWants);
+
+// function satbeerWants(item){
+//     alert(item + " satbeer wants");
+// }
+
+// arr = ["hello", "world", "nice", "things"];
+
+
+// function utkarshWants(item, i){
+//     console.log(item, i, " utkash want function");
+// }
+
+// arr.forEach(utkarshWants);
+
+// function forEach(fn){
+//     fn(item, index, arr);
+// }
+
+
+// 2 map :- --->
+// const arr = [1, 2, 3, 4];
+
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     console.log(element * 3);
+// }
+
+// function hemantTrippleFunction(item){
+//     return item * 3;
+// }
+
+// const mappedArray = arr.map(hemantTrippleFunction);
+// console.log(mappedArray, "mapped array1");
+// console.log(arr, " Original array");
