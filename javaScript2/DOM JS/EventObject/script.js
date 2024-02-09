35b2b184a2c767da4c75d9387a4eb5733c4535fa
Hemant <hemantkumarsonkar1@gmail.com>
2024-02-09 19:26:52 +0530

    Event Object

---
+++ javaScript2/DOM JS/EventObject/script.js
@@ -0,0 +1,25 @@
+// const button1Element = document.getElementById("button1");
+
+// button1Element.addEventListener("click", (eventObj) => {
+//     console.log("button is clicked !!!");
+//     console.log(eventObj);
+// }); 
+
+//  --------------------- IMPORTANT THING ABOUT EVENT OBJ ---------------------
+
+// const containerElement = document.getElementById("container");
+// containerElement.addEventListener("mouseenter", (event) => {
+//     containerElement.style.background = "red";
+//     console.log("mouseenter", event);
+//     console.log(event.target);
+// });
+
+// containerElement.addEventListener("mouseout", (event) => {
+//     containerElement.style.background = "yellow";
+// });
+
+
+// window.addEventListener("resize", (evenObj) => {
+//     console.log(evenObj);
+//     console.log("resize");
+// });
