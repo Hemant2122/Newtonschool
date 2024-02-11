60c5eb233ccdb5f6029d52edb2b5c55b648f1b8d
Hemant <hemantkumarsonkar1@gmail.com>
2024-02-11 17:29:33 +0530

    bubbling and capturing phase

--- javaScript2/DOM JS/BubblingAndCapturing/script.js
+++ javaScript2/DOM JS/BubblingAndCapturing/script.js
@@ -1,15 +1,57 @@
+
+// ------------------ BUBBLING PHASE --------------------
+
+// const grandParentElement = document.getElementById("grandParent");
+// const parentElement = document.getElementById("parent");
+// const childElement = document.getElementById("child");
+
+// childElement.addEventListener("click", () => {
+//     console.log("child Clicked !!!");
+// });
+
+// parentElement.addEventListener("click", () => {
+//     console.log("Parent Clicked !!!!!!......");
+// });
+
+// grandParentElement.addEventListener("click", () => {
+//     console.log("grandParent Clicked !!!!!!......");
+// });
+
+
+// ------------------------ CAPTURING PHASE --------------------
+
 const grandParentElement = document.getElementById("grandParent");
 const parentElement = document.getElementById("parent");
 const childElement = document.getElementById("child");
 
-childElement.addEventListener("click", () => {
-    console.log("child Clicked !!!");
-});
+// childElement.addEventListener("click", (eventObj) => {
+//     console.log("child hello !!!...");
+// }, true);
 
-parentElement.addEventListener("click", () => {
-    console.log("Parent Clicked !!!!!!......");
-});
+// grandParentElement.addEventListener("click", (eventObj) => {
+//     console.log(eventObj.target);
+//     console.log("grandParent hello !!!.......");
+// }, true);
+
+// parentElement.addEventListener("click", (eventObj) => {
+//     console.log("parent hello !!!......");
+// }, true);
+
+// Question:----
+
+childElement.addEventListener("click", () => {
+    console.log("child hello !!!...");
+}, false);
 
 grandParentElement.addEventListener("click", () => {
-    console.log("grandParent Clicked !!!!!!......");
-});
+    console.log("grandParent hello !!!.......");
+}, true);
+
+parentElement.addEventListener("click", () => {
+    console.log("parent hello !!!......");
+}, false);
+
+
+//  Answer : ---------
+
+// grandParent hello !!!....... ,  child hello !!!... , parent hello !!!......
