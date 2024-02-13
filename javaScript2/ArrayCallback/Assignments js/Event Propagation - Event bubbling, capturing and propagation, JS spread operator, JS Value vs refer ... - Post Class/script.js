cabf028173d0cc12c4b81bdcf2b4f8310bb1a839
Hemant <hemantkumarsonkar1@gmail.com>
2024-02-13 17:56:39 +0530

    event bubbling and capturing

---
+++ javaScript2/ArrayCallback/Assignments js/Event Propagation - Event bubbling, capturing and propagation, JS spread operator, JS Value vs refer ... - Post Class/script.js
@@ -0,0 +1,17 @@
+const checkBox = document.getElementById("check").value;
+
+
+
+function func1(event) {
+    console.log("DIV 1 is clicked.");
+    true;
+}
+
+function func2() {
+    console.log("DIV 2 is clicked.");
+}
+
+
+
+
+
