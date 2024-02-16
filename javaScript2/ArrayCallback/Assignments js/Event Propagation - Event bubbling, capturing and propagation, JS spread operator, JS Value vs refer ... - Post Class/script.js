9e4f21517ab2905348313c31c2e7651907d22e9c
Hemant <hemantkumarsonkar1@gmail.com>
2024-02-16 15:54:59 +0530

    Shopping cart

--- javaScript2/ArrayCallback/Assignments js/Event Propagation - Event bubbling, capturing and propagation, JS spread operator, JS Value vs refer ... - Post Class/script.js
+++ javaScript2/ArrayCallback/Assignments js/Event Propagation - Event bubbling, capturing and propagation, JS spread operator, JS Value vs refer ... - Post Class/script.js
@@ -1,17 +1,18 @@
-const checkBox = document.getElementById("check").value;
-
-
 
 function func1(event) {
     console.log("DIV 1 is clicked.");
-    true;
 }
 
 function func2() {
     console.log("DIV 2 is clicked.");
 }
 
+const checkBoxElement = document.getElementById("check");
 
-
+checkBoxElement.addEventListener("click", (eventObj) => {
+    const div1Element = document.getElementById("div1");
+    div1Element.append = eventObj.stopPropagation;
+    
+});
 
 
