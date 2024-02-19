1cc10f64ca8bd9c3075073ab3ac11b864658e952
Hemant <hemantkumarsonkar1@gmail.com>
2024-02-19 16:01:34 +0530

    REGEX - Name and Number

--- javaScript2/ArrayCallback/ClassLecture2023july/EVENTS/6. FormEvent/script.js
+++ javaScript2/ArrayCallback/ClassLecture2023july/EVENTS/6. FormEvent/script.js
@@ -1,4 +1,4 @@
-const formElement = document.getElementById("form");
+// const formElement = document.getElementById("form");
 // console.log(formElement);
 
 // formElement.addEventListener("submit", (event) => {
@@ -17,7 +17,7 @@ const formElement = document.getElementById("form");
 //     event.preventDefault();
 // });
 
-const nameElement = document.getElementById("name");
+// const nameElement = document.getElementById("name");
 // nameElement.addEventListener("focus", (event) => {
 //     console.log("focus");
 // });
@@ -29,9 +29,108 @@ const nameElement = document.getElementById("name");
 
 // 3 -> key event -> keydown , keyup , keypress
 
-nameElement.addEventListener("keypress", (event) => {
-    console.log("button clicked");
-    console.log(event);
-    const key = event.key;
-    console.log(key);
+// nameElement.addEventListener("keypress", (event) => {
+//     console.log("button clicked");
+//     console.log(event);
+//     const key = event.key;
+//     console.log(key);
+// });
+
+
+const nameElement = document.getElementById("name");
+
+// Example 1 :---
+
+// nameElement.addEventListener("change", (event) => {
+//     console.log(event);
+//     const targetElement = event.target;
+//     targetElement.value = String(targetElement.value).toUpperCase();
+// });
+
+// Example 2 :-----
+
+// nameElement.addEventListener("keyup", (event) => {
+//     const targetElement = event.target;
+//     const value = String(targetElement.value);
+
+//     targetElement.value = value.substring(0, 1).toUpperCase() + value.substring(1).toLowerCase();
+// });
+
+
+// ------------------------ REGEX ----------------------------
+
+const formElement = document.getElementById("form");
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formElem = event.target;
+    console.dir(formElem); 
+    const elements = formElem.elements;
+    console.dir(elements);
+
+    // Elements 
+    const nameElement = elements.name;
+    const phoneNumberElemrnt = elements.phone;
+    const emailElement = elements.email;
+    const standardElement = elements.standard;
+
+    // Value 
+    const name = nameElement.value;
+    const phone = phoneNumberElemrnt.value;
+    const email = emailElement.value;
+    const standard = standardElement.value;
+
+    // Validation
+    // Name ->
+    const nameValidation = nameValidator(name);
+    if(!nameValidation){
+        console.log("Name is requied and It should be according to the rules!!!");
+        return;
+    }
+
+    // Number 
+    const numberValidation = phoneNumberValidator(phone);
+    if(!numberValidation){
+        console.log("Phone Number is requied 10 Number and It should be according to the rules!!!");
+        return;
+    }
+
+    const payload = {
+        name : name,
+        phone : phone,
+        email : email,
+        standard : standard
+    }
+    console.log(payload, "payload");
+
+    fetch("hemant.com", {method : "post", body: JSON.stringify(payload)});
+
+    // --------------- name Validation ----------------
+
+    function nameValidator(name){
+        const nameRegex = /^[a-zA-Z]+$/;
+        const Validate = name.match(nameRegex);
+
+        if(Validate){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // -------------------- Phone Number Validation --------------------
+
+    function phoneNumberValidator(phone){
+        const phoneRegex = /^[0-9]{10}$/;
+        const validateNumbet = String(phone).match(phoneRegex);
+
+        if(validateNumbet){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    
 });
