a3a859afb4d8bbfa5c1c499ad68977da9ce23b7e
Hemant <hemantkumarsonkar1@gmail.com>
2024-02-19 17:52:04 +0530

    null

--- javaScript2/ArrayCallback/Assignments js/Event Propagation - Event bubbling, capturing and propagation, JS spread operator, JS Value vs refer ... - Post Class/script.js
+++ javaScript2/ArrayCallback/Assignments js/Event Propagation - Event bubbling, capturing and propagation, JS spread operator, JS Value vs refer ... - Post Class/script.js
@@ -1,21 +1,13 @@
 
 function func1(event) {
     console.log("DIV 1 is clicked.");
+    if (document.getElementById('check').checked) {
+        event.stopPropagation();
+    }
+    event.preventDefault();
 }
 
 function func2() {
     console.log("DIV 2 is clicked.");
+
 }
-
-const checkBoxElement = document.getElementById("check");
-
-checkBoxElement.addEventListener("check", (eventObj) => {
-    const div1Element = document.getElementById("div1");
-    div1Element.addEventListener("click", () => {
-        function func1(event) {
-            console.log("DIV 1 is clicked.");
-            event.stopPropagation();
-        }
-    });
-    
-});
