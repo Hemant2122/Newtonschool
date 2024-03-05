d7aeddef119521dd76b216db35c86f0012631769
Hemant <hemantkumarsonkar1@gmail.com>
2024-03-05 14:02:38 +0530

    js Range balancer

---
+++ javaScript2/ArrayCallback/Assignments js/JS Range Balancer/script.js
@@ -0,0 +1,13 @@
+const numberBoxElement = document.getElementById("max-sum-holder-input");
+const inputRangeA = document.getElementById("range-a-holder-input");
+const inputRangeB = document.getElementById("range-b-holder-input");
+const rangeValueA = document.getElementById("range-a-value");
+const rangeValueB = document.getElementById("range-b-value");
+const sumElement = document.getElementById("sum");
+
+// Logic
+
+numberBoxElement.addEventListener("change", (event) => {
+    const value = event.target.value;
+    
+});
